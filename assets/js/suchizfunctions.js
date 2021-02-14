a46119400100d029c5e031ba517a3b30eb8d49a7
suchiz <kyuuzuka@hotmail.fr>
2021-02-14 02:53:51 +0100

    mail backend

--- assets/js/suchizfunctions.js
+++ assets/js/suchizfunctions.js
@@ -102,3 +102,31 @@ function changeDefOver(e) {
 function changeDefOut(e) {
   e.target.classList.toggle('opacity-toggle');
 }
+
+
+document.getElementById("contact-form").addEventListener("submit", submitForm);
+
+function submitForm() {
+
+    let name = document.getElementById("form-name").value;
+    let email = document.getElementById("form-email").value;
+    let message = document.getElementById("form-message").value;
+
+
+    // document.querySelector("contact-form").reset();
+    sendEmail(name, email, message);
+    document.getElementById("contact-form").reset();
+  }
+
+  function sendEmail(name, email, message){
+      Email.send({
+          Host: "smtp.gmail.com",
+          Username: 'suchizgames@gmail.com',
+          Password: "gjwhxavkhmjzmcnu",/*"gjwhxavkhmjzmcnu"*/
+          To: "suchizgames@gmail.com",
+          From: "suchizgames@gmail.com",
+          Subject: `${name} sent you a message`,
+          Body: `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`,
+      }).then((message) => alert("Mail is sent successfully :). Thank you !"));
+
+  }
--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,17 +1,12 @@
-<?php
+<?php 
     $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $meessage = $_POST['message']
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-    $email_from = 'suchizgames@gmail.com'
-    $email_subject = "Form from website"
-    $email_body = "User name: $name.\n".
-                    "User email: $visitor_email.\n".
-                    "User message: $meessage.\n";
-    $to = "suchizgames@gmail.com";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
-
-    mail($to, $email_subject, $email_body, $headers)
-    header("Location: index.html");
+    $formcontent="From: $name \n Message: $message";
+    $recipient = "suchizgames@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
 ?>
